935980d0f74ce541b78c2db3196be69a82e13be7
zoki <tosevzoran@gmail.com>
2023-11-25 19:38:39 +0100

    add autocomplete support

--- init.lua
+++ init.lua
@@ -1,5 +1,8 @@
 require "zoki.options" 
 require "zoki.keymaps"
 require "zoki.plugins"
+require "zoki.completion"
+
+
 
 vim.cmd "colorscheme codemonkey"
--- lua/zoki/options.lua
+++ lua/zoki/options.lua
@@ -41,7 +41,7 @@ local options = {
   scrolloff = 8,                           -- is one of my fav
   sidescrolloff = 8,
   guifont = "monospace:h17",               -- the font used in graphical neovim applications
-
+  relativenumber = true                    -- use relative line numbers
 }
 
 vim.opt.shortmess:append "c"
