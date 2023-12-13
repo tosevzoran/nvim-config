063cf953a01c7e090966a450e12207cee7b0f035
Zoran Tošev <zoran.tosev@symphony.is>
2023-12-13 20:04:43 +0100

    Setup the night fox colorscheme

--- lua/zoki/colorscheme.lua
+++ lua/zoki/colorscheme.lua
@@ -1,8 +1,26 @@
-vim.cmd [[
-try
-  colorscheme nightfox
-catch /^Vim\%((\a\+)\)\=:E185/
-  colorscheme default
-  set background=dark
-endtry
-]] 
+local isOk, nightfox = pcall(require, "nightfox")
+if not isOk then
+  vim.notify("nightfox not loaded")
+  return
+end
+
+nightfox.setup({
+  options = {
+    dim_inactive = true,
+    styles = {
+      comments = "italic",
+      keywords = "bold",
+      functions = "italic,bold",
+    }
+  }
+})
+-- vim.cmd [[
+-- try
+--  colorscheme nightfox
+-- catch /^Vim\%((\a\+)\)\=:E185/
+--   colorscheme default
+--   set background=dark
+-- endtry
+-- ]] 
+
+vim.cmd("colorscheme nightfox")
