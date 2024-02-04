fec3b3646644cec7a0cb260aa9f3d988b08eeb6f
zoki <tosevzoran@gmail.com>
2024-02-04 15:20:05 +0100

    Setup cssls

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -86,6 +86,10 @@ return {
         capabilities = capabilities,
       })
 
+      lspconfig.cssls.setup({
+        capabilities = capabilities,
+      })
+
       vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
       vim.keymap.set("n", "<leader>gd", vim.lsp.buf.definition, {})
       vim.keymap.set("n", "<leader>gr", vim.lsp.buf.references, {})
