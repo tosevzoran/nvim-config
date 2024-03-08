a7ebfac178a8cd5cfc0c0c2b7a9767f09cf568eb
Zoran Tošev <zoran.tosev@symphony.is>
2024-03-08 13:54:35 +0100

    lualine file name config

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -2,6 +2,15 @@ return {
 	"nvim-lualine/lualine.nvim",
 	dependencies = { "nvim-tree/nvim-web-devicons" },
 	config = function()
-		require("lualine").setup()
+		require("lualine").setup({
+			sections = {
+				lualine_c = {
+					{
+						"buffers",
+						show_filename_only = false,
+					},
+				},
+			},
+		})
 	end,
 }
