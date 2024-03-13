64efec90de47eae9e4210094ea581f417ff3a9db
Zoran Tošev <zoran.tosev@symphony.is>
2024-03-13 11:19:40 +0100

    show hidden files in telescope

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -23,6 +23,11 @@ return {
 	},
 	config = function()
 		require("telescope").setup({
+			pickers = {
+				find_files = {
+					hidden = true,
+				},
+			},
 			extensions = {
 				["ui-select"] = {
 					require("telescope.themes").get_dropdown({}),
