ab799e2b9a721bc8a864be54a5fe3408522d49ef
Zoran Tošev <zoran.tosev@symphony.is>
2024-03-18 08:16:33 +0100

    mappings for diagnostics

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -217,6 +217,10 @@ return { -- LSP Configuration & Plugins
 				--  For example, in C this would take you to the header
 				map("gD", vim.lsp.buf.declaration, "[G]oto [D]eclaration")
 
+				map("<leader>[", vim.diagnostic.open_float, "Open diagnostics")
+
+				map("[d", vim.diagnostic.goto_prev, "Diagnostics prev")
+				map("]d", vim.diagnostic.goto_next, "Diagnostics next")
 				-- The following two autocommands are used to highlight references of the
 				-- word under your cursor when your cursor rests there for a little while.
 				--    See `:help CursorHold` for information about when this is executed
