0cf1b299aab2c0240e42576c2cb40eedb8d740b8
Zoran Tošev <zoran.tosev@symphony.is>
2024-09-15 18:21:45 +0200

    neo-tree follow current file conf

--- lua/plugins/neo-tree.lua
+++ lua/plugins/neo-tree.lua
@@ -16,20 +16,26 @@ return {
 		-- "3rd/image.nvim", -- Optional image support in preview window: See `# Preview Mode` for more information
 	},
 	config = function()
-		--    require("neo-tree").setup({
-		--        filesystem = {
-		--          filtered_items = {
-		--            --visible = true,
-		--            hide_dotfiles = false,
-		--            hide_gitignored = true,
-		--            hide_by_name = {
-		--              ".github",
-		--              ".gitignore",
-		--            },
-		--            never_show = { ".git" },
-		--          },
-		--        },
-		--    })
+		require("neo-tree").setup({
+			filesystem = {
+				--          filtered_items = {
+				--            --visible = true,
+				--            hide_dotfiles = false,
+				--            hide_gitignored = true,
+				--            hide_by_name = {
+				--              ".github",
+				--              ".gitignore"
+				--            },
+				--            never_show = { ".git" },
+				--          },
+				use_libuv_file_watcher = {
+					enabled = true,
+				},
+				follow_current_file = {
+					enabled = true,
+				},
+			},
+		})
 
 		vim.keymap.set("n", "<leader>e", ":Neotree toggle right<CR>", { desc = "Toggle explorer" })
 		-- vim.keymap.set("n", "<leader>g", ":Neotree float git_status<CR>", { desc = "Toggle git status window" })
